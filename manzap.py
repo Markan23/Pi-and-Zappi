d060264dafad8eabec35a50bcf9b58ea67e32b16
Markan23 <32030440+Markan23@users.noreply.github.com>
2022-11-25 19:07:07 +0000

    Update manzap.py

--- manzap.py
+++ manzap.py
@@ -4,19 +4,30 @@ import requests
 from   requests.auth import HTTPDigestAuth
 import json
 import requests
+import RPi.GPIO as GPIO
 time.sleep(10)
+switch = 0
+loop = 0
 
-username    = '(serial number of Harvi)'
-password    = '(Password for Harvi)'
+Red_LED_PIN = 17
+Green_LED_PIN = 15
+Blue_LED_PIN = 13
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(Red_LED_PIN, GPIO.OUT)
+GPIO.setup(Green_LED_PIN, GPIO.OUT)
+GPIO.setup(Blue_LED_PIN, GPIO.OUT)
+
+username    = '<Harvi Serial Number>'
+password    = '<Harvi Password>'
 eddi_url    = 'https://s18.myenergi.net/cgi-jstatus-E'
 zappi_url   = 'https://s18.myenergi.net/cgi-jstatus-Z'
 harvi_url   = 'https://s18.myenergi.net/cgi-jstatus-H'
 status_url  = 'https://s18.myenergi.net/cgi-jstatus-*'
 dayhour_url = 'https://s18.myenergi.net/cgi-jdayhour-Z15536718-2021-10-20'
 
-switch = 0
-loop = 0
-
+#define a function to access the server using a parsed URL
 def access_server(url_request):
   headers = {'User-Agent': 'Wget/1.14 (linux-gnu)'}
   r = requests.get(url_request, headers = headers, auth=HTTPDigestAuth(username, password), timeout=10)
@@ -32,22 +43,34 @@ def access_server(url_request):
   return r.json()
 
 # end-of-function definition
-res = requests.post("http://192.168.1.xxx/cm?cmnd=Power%20Off")
-res = requests.post("http://192.168.1.xxx/cm?cmnd=Power%20Off")
+res = requests.post("http://192.168.1.251/cm?cmnd=Power%20Off")
+res = requests.post("http://192.168.1.252/cm?cmnd=Power%20Off")
 
 while loop == 0:
-    print("Start of outer loop")
+    GPIO.output(Blue_LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(Blue_LED_PIN, GPIO.LOW)
+    time.sleep(1)
+    print("Flash blue led")
     response_data = access_server(harvi_url)
     for item in response_data['harvi']:
        Solarp =  item['ectp1']
        if Solarp < 50:
           Solarp = 0
-		  switch = 0
-		  
-    while Solarpp != 0:
-	    print("Start of inner loop")
+          switch = 0
+
+    while Solarp != 0:
+        GPIO.output(Green_LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(Red_LED_PIN, GPIO.LOW)
+        time.sleep(1)
+        print("Green leed on")
+        print("Red led is off")
         response_data = access_server(zappi_url)
         for item in response_data['zappi']:
+            print ("Query date: ", item['dat'])
+            print ("Query date: ", item['dat'])
+            print ("Query time: ", item['tim'])
             Gridv = item['vol']/10
             Gridf = item['frq']
             Gridp = item['grd']
@@ -58,12 +81,13 @@ while loop == 0:
            if Solarp < 50:
               Solarp = 0
 
+
         house = Gridp+Solarp
         Excessp = Solarp-house
         if Solarp-house <=0:
            Excessp = 0
-        print ("Grid Voltage:",Gridv,"Vac")
-        print ("Grid Frequency:",Gridf,"Hz")
+        #print ("Grid Voltage:",Gridv,"Vac")
+        #print ("Grid Frequency:",Gridf,"Hz")
         print ("Grid Power:",Gridp,"W")
         print ("Solar Power:",Solarp,"W")
         print ("House is using: ", house,"W")
@@ -71,25 +95,31 @@ while loop == 0:
 
         if  switch == 1 and (Gridp < -300):
             print("Heater 2 on")
-            res = requests.post("http://(ip address of sonoff socket 2)/cm?cmnd=Power%20On")
+            res = requests.post("http://192.168.1.251/cm?cmnd=Power%20On")
             switch = 2
 
         if  switch == 0 and (Gridp < -300):
             print("Heater 1 on")
-            res = requests.post("http://(ip address of sonoff socket 1)/cm?cmnd=Power%20On")
+            res = requests.post("http://192.168.1.252/cm?cmnd=Power%20On")
             switch = 1
             time.sleep(5)
-
+		
         if  switch == 2 and (Gridp > 200):
             print("Heater 2 off")
-            res = requests.post("http://(ip address of sonoff socket 2)/cm?cmnd=Power%20Off")
+            res = requests.post("http://192.168.1.251/cm?cmnd=Power%20Off")
             switch = 1
             time.sleep(5)
 
         if  switch == 1 and (Gridp > 200):
             print("Heater 1 off")
-            res = requests.post("http://(ip address of sonoff socket 1)/cm?cmnd=Power%20Off")
+            res = requests.post("http://192.168.1.252/cm?cmnd=Power%20Off")
             switch = 0
-        # End of inner loop
         time.sleep(20)
+    GPIO.output(Red_LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(Green_LED_PIN, GPIO.LOW)
+    time.sleep(1)
+    print("Red LED is on")
+    print("Green led is off")
     time.sleep(600)
+
